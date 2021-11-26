5ff2afc17164aff227aa3000ba4d2ef58f26f037
Leothi <leohb2@gmail.com>
2021-11-26 15:53:40 -0300

    refactoring api

--- project/api/utils/logger.py
+++ project/api/utils/logger.py
@@ -3,19 +3,15 @@ from loguru import logger
 # Logging with string allignment
 time = '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>'
 level = "<level>{level: ^18}</level>"
-function = "<cyan>{name: <32}</cyan>:<cyan>{function: ^30}</cyan>:<cyan>{line: 5}</cyan>"
+function = "<cyan>{name: <40}</cyan>:<cyan>{function: ^30}</cyan>:<cyan>{line: 5}</cyan>"
 message = "<bold>{message}</bold>"
 
 DEFAULT_FORMAT = ' | '.join([time, level, function, message])
 
 
 # Personalized logging
-def log_request(
-        level: str, method: str, id: str, ip: str, endpoint: str,
-        tempo: str = None, local: bool = False):
+def log_request(level: str, method: str, endpoint: str, time: str = None):
     message = f"{method: ^4} | ENDPOINT: {endpoint: ^20}"
-    if not local:
-        message = f"{method: ^4} | ID: {id: <36} | IP: {ip: ^16} | ENDPOINT: {endpoint: ^20}"
-    if tempo:
-        message = ' | '.join([message, f"TIME: {tempo}"])
+    if time:
+        message = ' | '.join([message, f"TIME: {time}"])
     logger.log(level, message)
