e054c2c016188acbc94f6a5ffc7280c2df8812e9
Leothi <leohb2@gmail.com>
2023-05-23 14:53:45 -0300

    adding docstring to sum function

--- project/api/modules/math.py
+++ project/api/modules/math.py
@@ -1,2 +1,5 @@
-def sum_two_numbers(x, y):
+from typing import Union
+
+def sum_two_numbers(x: Union[float, int], y: Union[float, int]) -> Union[float, int]:
+    "Returns the sum of two numbers, that could be either a float or an int"
     return x + y
